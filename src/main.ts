2373569f7a5a33f811c40e87f62bce72cd913756
Paul Boocock <paul@snowplowanalytics.com>
2020-07-24 20:32:14 +0100

    send event

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,28 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
+import {snowplow} from 'snowplow-tracker'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    const emitter = snowplow.emitter(
+      'fdd71f08e6b6.ngrok.io', // Collector endpoint
+      'http', // Optionally specify a method - http is the default
+      8080, // Optionally specify a port
+      'POST', // Method - defaults to GET
+      5, // Only send events once n are buffered. Defaults to 1 for GET requests and 10 for POST requests.
+      function (e: string) {
+        // Callback called for each request
+        if (e) {
+          throw e
+        }
+      },
+      {maxSockets: 6} // Node.js agentOptions object to tune performance
+    )
+    const tracker = snowplow.tracker([emitter], 'myTracker', 'myApp', false)
+    tracker.trackUnstructEvent({
+      schema: 'iglu:com.snowplowanalytics.iglu/anything-a/jsonschema/1-0-0',
+      data: github.context
+    })
   } catch (error) {
     core.setFailed(error.message)
   }
